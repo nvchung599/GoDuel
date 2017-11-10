34ca4b6d38961598178c04b30b2a8e4b7fb13cc7
nvchung599 <nvchung599@gmail.com>
2017-11-10 00:32:10 -0800

    added boundary physics, after burners, graphics, and guns
    game play mechanics should be pretty spot on with continuum now

--- mygame.py
+++ mygame.py
@@ -1,21 +1,43 @@
 from objects import *
 
 
+"""
+TRANSFORMING IMAGES
+
+DRAWING WITH PYGAME PERFORMS THE FOLLOWING TRANSFORM:
+
+U     1  0
+V  *  0 -1
+
+WE MUST COMPENSATE WITH A PRE-TRANSFORM TO END UP WITH A CORRECT IMAGE IN THE VIEWABLE WINDOW
+
+U     1  0     0
+V  *  0 -1  -  WIDTH
+
+UPDATE THIS EVERY TICK
+
+"""
+
+
 class MyGame(object):
 
     def __init__(self):
         pygame.init()
-        self.width = 800
-        self.height = 800
+        self.width = 1200
+        self.height = 1200
         self.screen = pygame.display.set_mode((self.width, self.height))
-        self.FPS = 60 # TODO figure out time scale
+        self.FPS = 100 # TODO figure out time scale
         self.clock = pygame.time.Clock()
         self.bg_color = (0, 0, 0)
 
         # TODO automate player placement in duel box
         self.players = []
-        self.players.append(Player(1, [0, 0]))
-        self.players.append(Player(2, [self.width/2, -self.height/2]))
+        self.players.append(Player(1, [100, 100], [self.width, self.height]))
+        #self.players.append(Player(2, [self.width/2, -self.height/2]))
+
+        self.bricks = []
+        self.boundaries = {'east':0, 'north':0, 'west':0, 'south':0} # should be 4 integer limits, with cardinal direction identity, for boundary in boundaries.items()
+        self.generate_duelbox()
 
     def run(self):
 
@@ -37,16 +59,148 @@ class MyGame(object):
 
                 player.current_action = self.keymap(keys, player.alliance)
                 player.maneuver(1/self.FPS)
+                player.shoot(self.screen)
 
                 # TODO center screen on player 1
                 # TODO create grid underlay
                 # TODO downsize warbird png, sync hitbox with image, alpha image
 
-                draw_centered(player.transform_image(), self.screen, player.transform_position())
+                # draw_centered(player.transform_image(), self.screen, player.transform_position())
+                player.draw(self.screen) # draws this player on the screen
+
+            if self.detect_collision():
+                self.detect_collision()
+
+            for brick in self.bricks:
+                brick.draw(self.screen)
 
             pygame.display.update()
             self.clock.tick(self.FPS)
             # TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+
+    def generate_duelbox(self):
+
+        graphic_width = 32
+
+        width_index = int(self.width/graphic_width)
+        height_index = int(self.height/graphic_width) # assumes wall graphic is 32 pixels wide
+
+        for w in range(1, width_index, 1):
+            for h in range(1, height_index, 1):
+                if w == 1 or h == 1 or w == width_index-1 or h == height_index-1:
+                    self.bricks.append(Brick([w * graphic_width, h * graphic_width], [self.width, self.height]))
+
+        # for self.boundaries.items():
+
+        self.boundaries['east'] = (width_index - 2.5) * graphic_width
+        self.boundaries['north'] = (height_index - 2.5) * graphic_width
+        self.boundaries['west'] = 2.5 * graphic_width
+        self.boundaries['south'] = 2.5 * graphic_width
+
+    def detect_collision(self):
+
+        damping_coefficient = 0.5
+
+        for player in self.players:
+
+            if player.position[0] < self.boundaries['west']: #left wall collision
+
+                delta_x = self.boundaries['west'] - player.position[0]
+                velocity_slope = player.velocity[1]/player.velocity[0]
+                delta_y = delta_x * velocity_slope
+                collision_coordinate = [player.position[0] + delta_x, player.position[1] + delta_y]
+
+
+                collision_penetration_ratio = math.sqrt(delta_x ** 2 + delta_y ** 2)/player.calc_speed()
+
+                new_velocity = [-player.velocity[0]*damping_coefficient, player.velocity[1]*damping_coefficient]
+
+                new_position = [collision_coordinate[0]+new_velocity[0]*collision_penetration_ratio,
+                                collision_coordinate[1]+new_velocity[1]*collision_penetration_ratio]
+
+                player.velocity = new_velocity
+                player.position = new_position
+
+            elif player.position[0] > self.boundaries['east']:  # left wall collision
+
+                delta_x = self.boundaries['east'] - player.position[0]
+                velocity_slope = player.velocity[1] / player.velocity[0]
+                delta_y = delta_x * velocity_slope
+                collision_coordinate = [player.position[0] + delta_x, player.position[1] + delta_y]
+
+                collision_penetration_ratio = math.sqrt(delta_x ** 2 + delta_y ** 2) / player.calc_speed()
+
+                new_velocity = [-player.velocity[0] * damping_coefficient, player.velocity[1] * damping_coefficient]
+
+                new_position = [collision_coordinate[0] + new_velocity[0] * collision_penetration_ratio,
+                                collision_coordinate[1] + new_velocity[1] * collision_penetration_ratio]
+
+                player.velocity = new_velocity
+                player.position = new_position
+
+            elif player.position[1] > self.boundaries['north']:  # left wall collision
+
+                delta_y = self.boundaries['north'] - player.position[1]
+                inv_velocity_slope = player.velocity[0] / player.velocity[1]
+                delta_x = delta_y * inv_velocity_slope
+                collision_coordinate = [player.position[0] + delta_x, player.position[1] + delta_y]
+
+                collision_penetration_ratio = math.sqrt(delta_x ** 2 + delta_y ** 2) / player.calc_speed()
+
+                new_velocity = [player.velocity[0] * damping_coefficient, -player.velocity[1] * damping_coefficient]
+
+                new_position = [collision_coordinate[0] + new_velocity[0] * collision_penetration_ratio,
+                                collision_coordinate[1] + new_velocity[1] * collision_penetration_ratio]
+
+                player.velocity = new_velocity
+                player.position = new_position
+
+            elif player.position[1] < self.boundaries['south']:  # left wall collision
+
+                delta_y = self.boundaries['south'] - player.position[1]
+                inv_velocity_slope = player.velocity[0] / player.velocity[1]
+                delta_x = delta_y * inv_velocity_slope
+                collision_coordinate = [player.position[0] + delta_x, player.position[1] + delta_y]
+
+                collision_penetration_ratio = math.sqrt(delta_x ** 2 + delta_y ** 2) / player.calc_speed()
+
+                new_velocity = [player.velocity[0] * damping_coefficient, -player.velocity[1] * damping_coefficient]
+
+                new_position = [collision_coordinate[0] + new_velocity[0] * collision_penetration_ratio,
+                                collision_coordinate[1] + new_velocity[1] * collision_penetration_ratio]
+
+                player.velocity = new_velocity
+                player.position = new_position
+
+
+            # elif player.position[1] < self.boundaries['south']:  # left wall collision
+            #
+            #     delta_x = self.boundaries['south'] - player.position[0]
+            #     velocity_slope = player.velocity[1] / player.velocity[0]
+            #     delta_y = delta_x * velocity_slope
+            #     collision_coordinate = [player.position[0] + delta_x, player.position[1] + delta_y]
+            #
+            #     collision_penetration_ratio = math.sqrt(delta_x ** 2 + delta_y ** 2) / player.calc_speed()
+            #
+            #     new_velocity = [-player.velocity[0] * damping_coefficient, player.velocity[1] * damping_coefficient]
+            #
+            #     new_position = [collision_coordinate[0] + new_velocity[0] * collision_penetration_ratio,
+            #                     collision_coordinate[1] + new_velocity[1] * collision_penetration_ratio]
+            #
+            #     player.velocity = new_velocity
+            #     player.position = new_position
+
+
+
+
+                # for item in self.boundaries.items():
+        #     print(item[1])
+
+            else:
+                return False
+
+        return True
+
 
     def keymap(self, keys, alliance): #add input for player 1 or 2
 
@@ -55,25 +209,27 @@ class MyGame(object):
             right_bool = keys[pygame.K_RIGHT]
             up_bool = keys[pygame.K_UP]
             down_bool = keys[pygame.K_DOWN]
-            # AFTERBURN
-            # TRIGGER
+            afterburn_bool = keys[pygame.K_LSHIFT]
+            trigger_bool = keys[pygame.K_LCTRL]
         elif alliance == 2:
             left_bool = keys[pygame.K_j]
             right_bool = keys[pygame.K_l]
             up_bool = keys[pygame.K_i]
             down_bool = keys[pygame.K_k]
-            # AFTERBURN
-            # TRIGGER
+            afterburn_bool = keys[pygame.K_g]
+            trigger_bool = keys[pygame.K_b]
         else:
             left_bool = False
             right_bool = False
             up_bool = False
             down_bool = False
-            # AFTERBURN
-            # TRIGGER
+            afterburn_bool = False
+            trigger_bool = False
 
         turn = 0
         throttle = 0
+        afterburn = 0
+        trigger = 0
 
         if not left_bool and not right_bool:
             turn = 0
@@ -93,7 +249,13 @@ class MyGame(object):
         elif up_bool:
             throttle = 1
 
-        actions = {'turn': turn, 'throttle': throttle}
+        if afterburn_bool:
+            afterburn = 1
+
+        if trigger_bool:
+            trigger = 1
+
+        actions = {'turn': turn, 'throttle': throttle, 'afterburn': afterburn, 'trigger': trigger}
 
         return actions
 
--- objects.py
+++ objects.py
@@ -1,24 +1,43 @@
 from general import *
 
 # GameObject
-# TangibleGameObject        AestheticGameObject
-# Player, Wall, Bullet      Exhaust, Bullet trail
+# Player, Wall, Bullet      Bullet trail
 
 
 class GameObject(object):
 
-    def __init__(self, object_image, object_position):
-        self.image = load_image_convert_alpha(object_image)
+    def __init__(self, object_image, object_position, object_screen_coord_sys):
+        self.image = load_image_convert_alpha(object_image) # the image's forward position should point up/north
+
+        self.screen_coord_sys = object_screen_coord_sys
+
         self.position = object_position
         self.velocity = [0, 0]
         self.angle = 0
+
+        self.transformed_position = None
+        self.transformed_velocity = None
+        self.transformed_angle = None
+
         self.alliance = -1
         # -1: aesthetic object
         # 0: neutral object w/ hit box
         # 1: team 1
         # 2: team 2
 
-    def transform_image(self):
+
+
+    def calc_speed(self):
+        return math.sqrt(self.velocity[0] ** 2 + self.velocity[1] ** 2)
+
+    def predraw_transform(self):
+        self.transformed_position = [self.position[0], -self.position[1] + self.screen_coord_sys[1]]
+        self.transformed_velocity = [self.velocity[0], self.velocity[1]*-1]
+        self.transformed_angle = -self.angle
+
+
+    # TODO remove this function from repeated execution, store image variable as transformed version
+    def transform_object_image(self):
         return rotate_center(self.image, self.angle - 90)
 
     def transform_position(self):
@@ -27,31 +46,59 @@ class GameObject(object):
 
 class Player(GameObject):
 
-    def __init__(self, alliance, position):
-        super().__init__('player_image.png', position)
+    def __init__(self, alliance, position, object_screen_coord_sys):
+        super().__init__('player_image.png', position, object_screen_coord_sys)
         self.angle = 90  # should coincide with image file
+        self.discreet_angle = 90
+
+        # graphics
+        self.image_small_exhaust = load_image_convert_alpha('small_exhaust.png')
+        self.image_medium_exhaust = load_image_convert_alpha('medium_exhaust.png')
+        self.image_large_exhaust = load_image_convert_alpha('large_exhaust.png')
 
         # maneuverability
         self.throttle_acceleration = 1000 # units/s^2
-        self.turn_speed = 270 # degrees/s
-        self.max_speed = 750  # units/s
-        self.current_action = {'throttle': 0, 'turn': 0, 'trigger': 0}
+        self.throttle_afterburn_acceleration = 2000
+        self.turn_speed = 350 # degrees/s
+        self.max_speed = 500  # units/s
+        self.max_afterburn_speed = 700
+        self.current_action = {'throttle': 0, 'turn': 0, 'trigger': 0, 'afterburn':0}
 
         # game settings
+        self.radius = 32
         self.alliance = alliance
+        self.shots = []
+        self.gun_cool_down_frames = 0
+
+    def predraw_transform(self):
+        self.transformed_position = [self.position[0], -self.position[1] + self.screen_coord_sys[1]]
+        self.transformed_velocity = [self.velocity[0], self.velocity[1]*-1]
+        self.transformed_angle = -self.discreet_angle
 
     def maneuver(self, time_between_frames):
 
         # input for either maneuver should be: -1, 0, 1
 
-        self.velocity[0] += math.cos(math.radians(self.angle))*self.throttle_acceleration*time_between_frames*self.current_action['throttle']
-        self.velocity[1] += math.sin(math.radians(self.angle))*self.throttle_acceleration*time_between_frames*self.current_action['throttle']
+        if self.current_action['afterburn']:
+            instantaneous_max_speed = self.max_afterburn_speed
+            instantaneous_accleration = self.throttle_afterburn_acceleration
+        else:
+            instantaneous_max_speed = self.max_speed
+            instantaneous_accleration = self.throttle_acceleration
+
+
+
+        self.velocity[0] += math.cos(math.radians(self.discreet_angle))*instantaneous_accleration*time_between_frames*self.current_action['throttle']
+        self.velocity[1] += math.sin(math.radians(self.discreet_angle))*instantaneous_accleration*time_between_frames*self.current_action['throttle']
 
         # max speed check and adjustment
 
-        instantaneous_speed = math.sqrt(self.velocity[0]**2 + self.velocity[1]**2)
-        if instantaneous_speed > self.max_speed:
-            speed_reduction = self.max_speed/instantaneous_speed
+
+
+        instantaneous_speed = self.calc_speed()
+
+        if instantaneous_speed > instantaneous_max_speed:
+            speed_reduction = instantaneous_max_speed/instantaneous_speed
             self.velocity[0] *= speed_reduction
             self.velocity[1] *= speed_reduction
 
@@ -59,6 +106,11 @@ class Player(GameObject):
         self.position[1] += self.velocity[1]*time_between_frames
 
         self.angle += self.turn_speed*time_between_frames*self.current_action['turn']
+        self.discreet_angle = round(self.angle/9)*9
+
+
+        for shot in self.shots:
+            shot.move(time_between_frames)
 
         # have current pos and vel
         # take input from mygame
@@ -71,7 +123,126 @@ class Player(GameObject):
         #     recalculate if wall collision
         #         detect collision again for corner case
 
-class Exhaust(GameObject):
-    def __init__(self, position, angle):
-        super().__init__('exhaust.png', position)
-        self.alliance = -1
+    def shoot(self, screen):
+
+        if self.current_action['trigger'] == 1 and self.gun_cool_down_frames == 0:
+            self.shots.append(Shot(self.position, self.velocity, self.discreet_angle, self.screen_coord_sys, screen))
+            self.gun_cool_down_frames = 20
+
+        elif self.gun_cool_down_frames > 0:
+            self.gun_cool_down_frames -= 1
+
+
+
+    def draw(self, screen):
+
+        self.predraw_transform()
+
+        draw_centered(self.transform_object_image(), screen, self.transformed_position)
+        #draw centered 64x64 image on position coordinates, which are corrected for -Y,
+
+        if self.current_action['throttle'] == 1:
+            if self.current_action['afterburn'] == 1:
+                rear_exhaust_image = rotate_center(self.image_large_exhaust, self.discreet_angle - 90)
+            else:
+                rear_exhaust_image = rotate_center(self.image_medium_exhaust, self.discreet_angle - 90)
+            rear_exhaust_position = [self.transformed_position[0]+self.radius*math.cos(math.radians(self.transformed_angle + 180)),
+                                     self.transformed_position[1]+self.radius*math.sin(math.radians(self.transformed_angle + 180))]
+            draw_centered(rear_exhaust_image, screen, rear_exhaust_position)
+
+        elif self.current_action['throttle'] == -1:
+            if self.current_action['afterburn'] == 1:
+                front_exhaust_image_1 = rotate_center(self.image_medium_exhaust, self.discreet_angle + 70)
+                front_exhaust_image_2 = rotate_center(self.image_medium_exhaust, self.discreet_angle + 110)
+            else:
+                front_exhaust_image_1 = rotate_center(self.image_small_exhaust, self.discreet_angle + 70)
+                front_exhaust_image_2 = rotate_center(self.image_small_exhaust, self.discreet_angle + 110)
+
+            front_exhaust_position_1 = [self.transformed_position[0]+self.radius*0.5*math.cos(math.radians(self.transformed_angle + 90)),
+                                        self.transformed_position[1]+self.radius*0.5*math.sin(math.radians(self.transformed_angle + 90))]
+            front_exhaust_position_2 = [self.transformed_position[0]+self.radius*0.5*math.cos(math.radians(self.transformed_angle - 90)),
+                                        self.transformed_position[1]+self.radius*0.5*math.sin(math.radians(self.transformed_angle - 90))]
+            draw_centered(front_exhaust_image_1, screen, front_exhaust_position_1)
+            draw_centered(front_exhaust_image_2, screen, front_exhaust_position_2)
+
+            # todo counter thrust graphic when turn is terminated
+        if self.current_action['turn'] != 0:
+            right_exhaust_image = rotate_center(self.image_small_exhaust, self.discreet_angle - 90 * self.current_action['turn'])
+            left_exhaust_image = rotate_center(self.image_small_exhaust, self.discreet_angle + 90 * self.current_action['turn'])
+
+            right_exhaust_position = [self.transformed_position[0] + self.radius * math.cos(math.radians(self.transformed_angle + 90)),
+                                      self.transformed_position[1] + self.radius * math.sin(math.radians(self.transformed_angle + 90))]
+            left_exhaust_position = [self.transformed_position[0] + self.radius * math.cos(math.radians(self.transformed_angle - 90)),
+                                     self.transformed_position[1] + self.radius * math.sin(math.radians(self.transformed_angle - 90))]
+
+            draw_centered(right_exhaust_image, screen, right_exhaust_position)
+            draw_centered(left_exhaust_image, screen, left_exhaust_position)
+
+        for shot in self.shots:
+            shot.draw()
+
+        # given throttle and turn states, also draw exhaust under player image
+
+class Bullet(GameObject):
+
+    def __init__(self, position, velocity, object_screen_coord_sys):
+        super().__init__('bullet.png', position, object_screen_coord_sys)
+        self.velocity = velocity
+
+    def move(self, time_between_frames):
+        self.position = [self.position[0] + self.velocity[0] * time_between_frames,
+                         self.position[1] + self.velocity[1] * time_between_frames]
+
+    def hit(self):
+        """
+        display graphic
+        :return:
+        """
+        None
+
+    def draw(self, screen):
+        self.predraw_transform()
+        draw_centered(self.transform_object_image(), screen, self.transformed_position)
+
+class Brick(GameObject):
+    def __init__(self, position, object_screen_coord_sys):
+        super().__init__('wall.png', position, object_screen_coord_sys)
+        self.width = 32 # half of player model
+
+    def draw(self, screen):
+        self.predraw_transform()
+        draw_centered(self.image, screen, self.position)
+
+
+class Shot(object):
+    def __init__(self, position, velocity, angle, object_screen_coord_sys, screen):
+
+        self.screen = screen
+
+        bullet_speed = 500
+
+        #right and left bullets
+        bullet_1_position = [position[0] + 25 * math.cos(math.radians(angle - 90)),
+                             position[1] + 25 * math.sin(math.radians(angle - 90))]
+        bullet_2_position = [position[0] + 25 * math.cos(math.radians(angle + 90)),
+                             position[1] + 25 * math.sin(math.radians(angle + 90))]
+
+        bullet_velocity = [bullet_speed * math.cos(math.radians(angle)) + velocity[0],
+                           bullet_speed * math.sin(math.radians(angle)) + velocity[1]]
+
+        self.bullet_1 = Bullet(bullet_1_position, bullet_velocity, object_screen_coord_sys)
+        self.bullet_2 = Bullet(bullet_2_position, bullet_velocity, object_screen_coord_sys)
+
+    def move(self, time_between_frames):
+        self.bullet_1.move(time_between_frames)
+        self.bullet_2.move(time_between_frames)
+        print(self.bullet_1.velocity)
+
+    def hit(self):
+        self.bullet_1.hit()
+        self.bullet_2.hit()
+
+    def draw(self):
+        self.bullet_1.draw(self.screen)
+        self.bullet_2.draw(self.screen)
+
